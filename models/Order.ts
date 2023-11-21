747469aa7a35f53222dfeb9a3c4f813921b5ec4f
Sam Frank <sff5097@nyu.edu>
2023-11-21 07:47:35 -0500

    update schema

--- models/Order.ts
+++ models/Order.ts
@@ -4,16 +4,16 @@ import mongoose from 'mongoose'
 export interface Orders extends mongoose.Document{
     buyer: number
     location: string
-    items: Record<string, string>[] 
-    createdAt: string
+    items: string[] 
+    createdAt: Date
     status: string
 }
 
 const OrderSchema = new mongoose.Schema({
   buyer: {type: Number, required: true},
   location: {type: String, required: true},
-  items: {type: [Object], required: true},
-  createdAt: {type: String, required: true},
+  items: {type: [], required: true},
+  createdAt: {type: Date, default: Date.now},
   status: {type: String, required: true}
 })
 
